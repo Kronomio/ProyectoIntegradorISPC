668c3eb66b4bd838646edc8008cacafa12e63a04
Cristian López <cristianlp.ok@gmail.com>
2022-08-10 11:53:42 -0300

    Se añadió la consulta 2 de la actividad de SQL

---
+++ Consulta 2.sql
@@ -0,0 +1,10 @@
+# Consulta 2: Inserte en la tabla correspondiente un nuevo animal (perro) 
+#como paciente y el dueño asociado a ese animal.
+
+insert into Dueno
+VALUES (33270795, "Ramiro", "Garcia", 3541658798, "Juan B. Justo 1900");
+
+INSERT INTO Perro
+VALUES (1, "Firulais", '2022-02-22', "Macho", 33270795);
+
+
