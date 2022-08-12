d0223589b3dda5c7c9b5e64041cd68210bdf06ec
Kwa <106926932+01Kwa@users.noreply.github.com>
2022-08-12 20:36:22 -0300

    Subida de Actividad 8
    Actividad n° 8, Historial4, de VALORACIÓN FINAL DEL MODULO

---
+++ Actividad8.py
@@ -0,0 +1,17 @@
+# ACTIVIDAD:
+# 8 ) Crear una tupla en Python con el nombre de “Historial4” la cual contenga los siguientes valores:
+#
+#         7510, 7960, 76180, 800, 4100
+#
+# Que representa montos de atención en pesos por diferentes servicios/consultas de la mascota “Olivia”. Cree una función para determinar el valor mínimo de atención gastada en “Olivia”  mostrándolo en pantalla.
+
+
+
+#Defino la variable Historial4
+Historial4 = [7510, 7960, 76180, 800, 4100] 
+
+#Creó la función valorMinimo, indica numero menor de Historial4.
+valorMinimo = min(Historial4)
+
+#Imprimó valorMinimo para que se muestre en pantalla.
+print("El menor monto en pesos por atención a Olivia es de: ",valorMinimo) 
